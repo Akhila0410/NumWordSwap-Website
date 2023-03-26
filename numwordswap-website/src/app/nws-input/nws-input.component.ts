bf0f7e19eb2eb46e49439fc7eb9627f6d62c8371
Akhila <akhila.dev0410@gmail.com>
2023-03-26 13:50:20 -0400

    Min and Max form validations

--- numwordswap-website/src/app/nws-input/nws-input.component.ts
+++ numwordswap-website/src/app/nws-input/nws-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { NwsService } from '../nws.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { NwsService } from '../nws.service';
 })
 export class NumSwapInputComponent implements OnInit {
 
-  maxNumber = new FormControl('');
+  maxNumber = new FormControl('maxNumber', [Validators.max(1000), Validators.min(1)]);
 
   constructor(private nwsService: NwsService) {
 
@@ -18,9 +18,11 @@ export class NumSwapInputComponent implements OnInit {
     this.maxNumber.valueChanges.subscribe(value => this.updateRequest(parseInt(value || "0")))
   }
   updateRequest(value: number) {
-    console.log('Entered new Max Number:', value);
-    this.nwsService.nwsRequest.maxNumber = value;
-    this.nwsService.getNwsResponse();
+    if (this.maxNumber.valid) {
+      console.log('Entered new Max Number:', value);
+      this.nwsService.nwsRequest.maxNumber = value;
+      this.nwsService.getNwsResponse();
+    }
   }
 
 }
